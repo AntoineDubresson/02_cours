f8bf93aa51f735cfde146c0c43b244cbe92d77cc
Antoine Dubresson <adubresson@ensc.fr>
2019-12-03 12:31:36 +0100

    dddd

--- 02_GitProjet/src/fr/adaming/entities/Produit.java
+++ 02_GitProjet/src/fr/adaming/entities/Produit.java
@@ -1,5 +1,98 @@
 package fr.adaming.entities;
 
 public class Produit {
+	
+	//Attributs
+	private long idProduit;
+	private String designation;
+	private String descripton;
+	private double prix; 
+	private int quantite;
+	private boolean selectionne;
+	private String photo;
+	
+	//Constructeur vide
+	public Produit() {
+		super();
+	}
+	
+	//Constructeur 
+	public Produit(long idProduit, String designation, String descripton, double prix, int quantite,
+			boolean selectionne, String photo) {
+		super();
+		this.idProduit = idProduit;
+		this.designation = designation;
+		this.descripton = descripton;
+		this.prix = prix;
+		this.quantite = quantite;
+		this.selectionne = selectionne;
+		this.photo = photo;
+	}
+
+	
+	//Getters et setters
+	public long getIdProduit() {
+		return idProduit;
+	}
+
+	public void setIdProduit(long idProduit) {
+		this.idProduit = idProduit;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public String getDescripton() {
+		return descripton;
+	}
+
+	public void setDescripton(String descripton) {
+		this.descripton = descripton;
+	}
+
+	public double getPrix() {
+		return prix;
+	}
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+	public int getQuantite() {
+		return quantite;
+	}
+
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+
+	public boolean isSelectionne() {
+		return selectionne;
+	}
+
+	public void setSelectionne(boolean selectionne) {
+		this.selectionne = selectionne;
+	}
+
+	public String getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	} 
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
